eb8eb025ecc52759dc0aed85eec895bfeccd2a74
Casey Harding <casey.r.harding@gmail.com>
2021-11-03 14:05:13 -0400

    Through to User component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=5')
+      .then(res => {
+        setUsers(res.data.results);
+      })
+      .catch(err => console.error(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, world!</h1>
     </div>
   );
 }
